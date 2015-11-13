1314f91a370b1d46edfdb42a12379050e82d8fc8
catonrug <aigars.kadikis@gmail.com>
2015-11-13 22:13:30 +0200

    Version 3
    all archive lines should end with [.7z] and not [7z]

--- get-all-bin-files.cmd
+++ get-all-bin-files.cmd
@@ -2,13 +2,13 @@
 set path=%path%;%~dp0
 
 set t=%temp%\psx
-set d=%~dp0playable
+set d=e:\psx
 if exist "%t%" rd "%t%" /Q /S
 if not exist "%t%" md %t%
 if exist "%d%" rd "%d%" /Q /S
 if not exist "%d%" md %d%
 dir /b /s > "%~dp0all.lst"
-sed -i "/7z$/!d" "%~dp0all.lst"
+sed -i "/\.7z$/!d" "%~dp0all.lst"
 sed -i "s/\d033/\^\d033/g" "%~dp0all.lst"
 
 for /f "tokens=*" %%a in ('type "%~dp0all.lst"') do (
@@ -16,10 +16,10 @@ for /f "tokens=*" %%n in ('echo "%%a" ^| sed "s/^.*\\\|\.7z.*$//g"') do (
 setlocal EnableDelayedExpansion
 echo %%n
 "%~dp07z.exe" x "%%a" -o"%t%" > nul 2>&1
-if not !errorlevel!==0 echo NOT OK %%n
+if not !errorlevel!==0 echo NOT OK %%n >> error.log
 "%~dp0unecm.exe" "%t%\Track 01.bin.ecm" "%d%\%%n.bin" > nul 2>&1
 if not !errorlevel!==0 (
-echo %%n is probably in "~t7z" direcotry
+echo %%n is probably in "~t7z" direcotry >> error.log
 move "%t%\*.bin" "%d%" > nul 2>&1
 if exist "%t%" rd "%t%" /Q /S > nul 2>&1
 if not exist "%t%" md "%t%" > nul 2>&1
@@ -28,5 +28,4 @@ del "%t%\Track 01.bin.ecm" /Q /F > nul 2>&1
 endlocal
 )
 )
-pause
-
+%systemroot%\System32\shutdown.exe -s -t 0
